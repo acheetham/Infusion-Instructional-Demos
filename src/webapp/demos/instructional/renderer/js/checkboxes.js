efaeaa013308162f7580b8f43237a1719d9c5c52
Anastasia Cheetham <ac@anastasiacheetham.ca>
2011-09-19 14:00:35 -0400

    FLUID-4145: Splittign the JS out into seperate files for renderer instructional demos

---
+++ src/webapp/demos/instructional/renderer/js/checkboxes.js
@@ -0,0 +1,66 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global demo:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+// Declare the demo namespace
+var demo = demo || {};
+
+(function ($) {
+    
+    // Define a component that will render the checkboxes
+    fluid.defaults("demo.checkboxRenderer", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+
+        selectors: {
+            checkboxRow: ".democ-checkboxes-checkbox",
+            box: ".democ-checkboxes-box",
+            checkboxLabel: ".democ-checkboxes-label"
+        },
+        repeatingSelectors: ["checkboxRow"],
+
+        model: {
+            checkboxes: {
+                selection: ["opt2", "opt3"],
+                choices: ["opt1", "opt2", "opt3", "opt4"],
+                names: ["Option 1", "Option 2", "Option 3", "Option 4"]
+            }
+        },
+
+        produceTree: "demo.checkboxRenderer.produceTree",
+
+        renderOnInit: true
+    });
+
+    // Define the function that will be used by the component to define the renderer component tree
+    demo.checkboxRenderer.produceTree = function (that) {
+        var tree = {
+            "expander": {
+                "type": "fluid.renderer.selection.inputs",
+                "rowID": "checkboxRow",
+                "labelID": "checkboxLabel",
+                "inputID": "box",
+                "selectID": "checkboxes",
+                "tree": {
+                    "selection": "${checkboxes.selection}",
+                    "optionlist": "${checkboxes.choices}",
+                    "optionnames": "${checkboxes.names}"
+                }
+            }
+        };
+        return tree;
+    };
+    
+})(jQuery);
---
+++ src/webapp/demos/instructional/renderer/js/radio.js
@@ -0,0 +1,66 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global demo:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+// Declare the demo namespace
+var demo = demo || {};
+
+(function ($) {
+    
+    // Define a component that will render the radio buttons
+    fluid.defaults("demo.radioRenderer", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+
+        selectors: {
+            radioRow: ".democ-radios-radio",
+            radioButton: ".democ-radios-radioButton",
+            radioLabel: ".democ-radios-radioLabel"
+        },
+        repeatingSelectors: ["radioRow"],
+
+        model: {
+            radios: {
+                selection: ["opt2"],
+                choices: ["opt1", "opt2", "opt3", "opt4"],
+                names: ["Option 1", "Option 2", "Option 3", "Option 4"]
+            }
+        },
+
+        produceTree: "demo.radioRenderer.produceTree",
+        
+        renderOnInit: true
+    });
+
+    // Define the function that will be used by the component to define the renderer component tree
+    demo.radioRenderer.produceTree = function (that) {
+        var tree = {
+            "expander": {
+                "type": "fluid.renderer.selection.inputs",
+                "rowID": "radioRow",
+                "labelID": "radioLabel",
+                "inputID": "radioButton",
+                "selectID": "radio-buttons",
+                "tree": {
+                    "selection": "${radios.selection}",
+                    "optionlist": "${radios.choices}",
+                    "optionnames": "${radios.names}"
+                }
+            }
+        };
+        return tree;
+    };
+    
+})(jQuery);
